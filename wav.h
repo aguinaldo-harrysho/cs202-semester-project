58375a8bb58bc3f36520c1178b4c5027eb3e1a41
aperezherrera <aperezherrera@nevada.unr.edu>
2021-12-02 22:29:09 -0800

    Added comment

--- wav.h
+++ wav.h
@@ -2,7 +2,9 @@
 #define WAV_H
 #include <string>
 #include "wave_header.h"
- 
+
+//wav.cpp should be responsible for getting the data that isn't the header. Will likely extend the header in some way.
+
 class Wav {
     //wav_header header;
 public:
