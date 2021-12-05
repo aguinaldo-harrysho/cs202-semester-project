cdf537463135b02cb0a92bf9e9a35013a13f9bc9
aguinaldo-harrysho <haguinaldo@nevada.unr.edu>
2021-12-05 13:35:25 -0800

    fixed gain files

--- gain.h
+++ gain.h
@@ -5,10 +5,5 @@
 class Gain
 {
     public:
-<<<<<<< HEAD
-    std::vector<int> process(std::vector<int>);
+    std::vector<float>process(std::vector<float>);
 };
-=======
-    std::vector<float> process(std::vector<float>);
-};
->>>>>>> 088d6f24bdcf3e8ec896df70032aa6183a2c33f9
