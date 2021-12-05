a1ab82e4b9eebabb65a2b6377837c129849461d9
aperezherrera <aperezherrera@nevada.unr.edu>
2021-12-04 20:07:49 -0800

    Added function that now combines the header data into the soundData when body data is read

--- wav.cpp
+++ wav.cpp
@@ -6,11 +6,41 @@
 #include "wav.h"
 #include "wave_body.h"
 
+wav_body Wav::combineHeaderAndBody(wav_header audiofile_header, wav_body audiofile_body){
+    // For loops are for atrributes that are arrays
+    for(int i = 0; i < 4; i++) audiofile_body.riff_header[i] = audiofile_header.riff_header[i]; // char riff_header[4]
+    audiofile_body.wav_size = audiofile_header.wav_size; // int wav_size
+    for(int i = 0; i < 4; i++) audiofile_body.wave_header[i] = audiofile_header.wave_header[i]; // char wave_header[4]
+    for(int i = 0; i < 4; i++) audiofile_body.fmt_header[i] = audiofile_header.fmt_header[i];// char fmt_header[4]
+    audiofile_body.fmt_chunk_size = audiofile_header.fmt_chunk_size;// int fmt_chunk_size
+    audiofile_body.audio_format = audiofile_header.audio_format;// short audio_format
+    audiofile_body.num_channels = audiofile_header.num_channels;// short num_channels
+    audiofile_body.sample_rate = audiofile_header.sample_rate;// int sample_rate
+    audiofile_body.byte_rate = audiofile_header.byte_rate;// int byte_rate
+    audiofile_body.sample_alignment = audiofile_header.sample_alignment;// short sample_alignment
+    audiofile_body.bit_depth = audiofile_header.bit_depth;// short bit_depth
+    for(int i = 0; i < 4; i++) audiofile_body.data_header[i] = audiofile_header.data_header[i];// char data_header[4]
+    audiofile_body.data_bytes = audiofile_header.data_bytes;// int data_bytes
+
+    return audiofile_body;
+}
+
 wav_body Wav::readBodyData(wav_header audiofile_header, std::string filename)
 {
     wav_body audiofile_body;
     int headerSize = 4 + (8 + audiofile_header.fmt_chunk_size) + 8 + 6;
     int bodySize = audiofile_header.wav_size - (headerSize - 8);
+
+    audiofile_body = combineHeaderAndBody(audiofile_header, audiofile_body);
+
+    if(audiofile_body.num_channels == 1)
+    {
+
+    }
+    else
+    {
+        
+    }
 
     audiofile_body.bytes.resize(bodySize);
     unsigned char buffer[bodySize];
