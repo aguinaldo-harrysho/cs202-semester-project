0c19eac2fbe64f2d4e426fa855ff6d663f3a5765
PerkinsDylan <dylanperkins@nevada.unr.edu>
2021-12-04 17:03:29 -0800

    Inital implementation of echo processor

---
+++ echo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "echo.h"
+
+std::vector<float> Echo::process(const std::vector<float>& input, float gain, int delay)
+{
+	std::vector<float> output;
+	output.reserve(input.size());
+	for(int i = 0; i < input.size(); i++)
+	{
+		if(i > delay) 
+		{
+			output.push_back(input[i] + gain*output[i - delay]);
+		
+		} else{
+			output.push_back(input[i]);
+		}
+	}
+	
+	return output;
+}
---
+++ echo.h
@@ -0,0 +1,12 @@
+#ifndef ECHO_H
+#define ECHO_H
+#include <vector>
+
+class Echo
+{
+
+public:
+	static std::vector<float> process(const std::vector<float>& input, float gain, int delay);
+};
+
+#endif
