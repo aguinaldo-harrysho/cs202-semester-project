b5e066355a5ca07ce8f3a06dbb9cb343a2cafe53
aperezherrera <aperezherrera@nevada.unr.edu>
2021-12-07 23:16:12 -0800

    Doxygen comments complete

--- wav.h
+++ wav.h
@@ -6,16 +6,31 @@
 #include "wave_header.h"
 #include "wave_body.h"
 
-//wav.cpp should be responsible for getting the data that isn't the header. Will likely extend the header in some way.
-
 /**
- * @brief This is the Normalizer class
+ * @brief This is the Wav class. Responsible for reading in and saving Wav data
  * 
  */
 class Wav {
 public:
+    /**
+     * @brief pulls the data from a wav_header into a wav_body
+     * @param audiofile_header - the wav_header object to pull from
+     * @param audiofile_body - the wav_body object to add wav_header data to
+     * @return wav_body - the wav_body object with header data
+     */
     static wav_body combineHeaderAndBody(wav_header, wav_body);
+    /**
+     * @brief creates a wav_body object with the audio data from the Wav file
+     * @param audiofile_header - the wav_header data of the file you wish to read from
+     * @param filename - the file to read audio data from
+     * @return wav_body - a wav_body object with header and body (audio) data
+     */
     static wav_body readBodyData(wav_header, std::string);
+    /**
+     * @brief saves and write data of a wav_body into a usable wav file
+     * @param audiofile_body - the wav_body data you wish to write into a file
+     * @param filename - the name of the file you wish to save it as
+     */
     static void writeAudiofile(wav_body, std::string); //Save a wav_body as an actual playable wav file
 };
  
