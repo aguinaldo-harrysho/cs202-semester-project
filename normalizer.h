829c1a4cdefb07d1abb24bca9556f188f5160972
aguinaldo-harrysho <haguinaldo@nevada.unr.edu>
2021-12-07 21:20:40 -0800

    added doxygen comments

--- normalizer.h
+++ normalizer.h
@@ -1,7 +1,15 @@
 #include <vector>
-
+/**
+ * @brief This is the Normalizer class
+ * 
+ */
 class Normalizer
 {
     public:
+    /**
+     * @brief Processor that normalizes the data
+     * @param data - Samples from the data section of the input file.
+     * @return std::vector<float>
+     */
     std::vector<float> process (std::vector<float>);
 };
