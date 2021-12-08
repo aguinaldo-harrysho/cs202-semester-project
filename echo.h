8665cc20da15a86a0f3325c22c8285d245649182
PerkinsDylan <dylanperkins@nevada.unr.edu>
2021-12-07 21:28:07 -0800

    added doxygen comments

--- echo.h
+++ echo.h
@@ -1,11 +1,19 @@
 #ifndef ECHO_H
 #define ECHO_H
 #include <vector>
-
+/**
+ * This is the Echo class
+ * 
+ */
 class Echo
 {
 
 public:
+	/**
+	* @brief Processor that adds an Echo
+	* 
+	* @param data - Samples from the data section of the input file.
+	*/
 	std::vector<float> process(std::vector<float> data);
 };
 
