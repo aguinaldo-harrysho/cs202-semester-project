8d8c7178adc43ae02293de206b037f24075b0818
aperezherrera <aperezherrera@nevada.unr.edu>
2021-12-07 22:39:47 -0800

    echo implemented correctly

--- echo.cpp
+++ echo.cpp
@@ -14,8 +14,12 @@ wav_body Echo::process(wav_body audiofile_body)
 
 	//std::vector<float> output;
 	//output.reserve(audiofile_body.monoChannel_sounData.size());
-	for(int i = 0; i < audiofile_body.monoChannel_sounData.size(); i++)
+	std::cout << "After Delay input" << std::endl;
+	std::cout << "Size: " << audiofile_body.monoChannel_sounData.size() << std::endl;
+	int stopSize = audiofile_body.monoChannel_sounData.size();
+	for(int i = 0; i < stopSize; i++)
 	{
+		//std::cout << i << std::endl;
 		if(i > delay) 
 		{
 			audiofile_body.monoChannel_sounData.push_back(audiofile_body.monoChannel_sounData[i] + gain*audiofile_body.monoChannel_sounData[i - delay]);
